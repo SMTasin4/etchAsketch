7ebac60bf0bdb3609fb2c299af36769c7d443f24
tasin <smt019953@gmail.com>
2024-04-02 15:22:17 +0600

    bug fixed with eraser function

--- js.js
+++ js.js
@@ -1,17 +1,37 @@
 const gridContainer = document.getElementById('gridContainer');
-createGrid(20,20)
-const boxs = document.querySelectorAll('.boxs')
-colorChanger()
 const popupBtn = document.querySelector('#popupBtn')
+popup()
 
 
+function colorChanger(){
+    const boxs = document.querySelectorAll('.boxs')
+    const color = document.getElementById('favcolor').value//color picker
+    boxs.forEach((box)=>{
+        box.addEventListener('click',(e)=>{
+                       
+            e.target.style.cssText = `background:${color};`
+
+        })
+    })
+}
+function eraser(){
+    const boxs = document.querySelectorAll('.boxs')
+    boxs.forEach((box)=>{
+        
+        box.addEventListener('contextmenu',(e)=>{
+            e.preventDefault()
+            e.target.style.cssText = 'background:white;'
+
+        })
+    })
+}
 
 function createGrid(rows,columns){
-    for(let i=0;i<=rows;i++){
-        const line = document.createElement('container')
+    for(let i=1;i<=rows;i++){
+        const line = document.createElement('div')
         gridContainer.appendChild(line)
         line.setAttribute('class','lines')
-        for(let j=0;j<=columns;j++){
+        for(let j=1;j<=columns;j++){
             const box = document.createElement('div')
             line.appendChild(box)
             box.setAttribute('class','boxs')
@@ -19,20 +39,26 @@ function createGrid(rows,columns){
     }
 }
 
-function colorChanger(){
-    boxs.forEach((box)=>{
-        box.addEventListener('click',(e)=>{
-            
-            e.target.style.cssText = 'background:red;'
-        })
-    })
-}
+
 function popup(){
     popupBtn.addEventListener('click',()=>{
         let columnNum = prompt('Enter the number of colums','50')
         let rowNum = prompt('Enter the number of rows','50')
-        if(columnNum==null || rowNum == null){
-
+        var mybox = document.querySelectorAll('.boxs')
+        if(mybox.length){
+            const lines = document.querySelectorAll('.lines')
+            lines.forEach(function(line){line.remove()})
+            const boxs = document.querySelectorAll('.boxs')
+            boxs.forEach(function(box){box.remove()})
+            createGrid(columnNum,rowNum)
+            colorChanger()
+            eraser()
+        }
+        else{
+            createGrid(columnNum,rowNum)
+            colorChanger()
+            eraser()
         }
     })
 }
+
